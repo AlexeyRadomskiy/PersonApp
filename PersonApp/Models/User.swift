cf1a00e79a8429eb7dc00df95d30fb88458c1faf
Alexey <alexeyradomskiy98@gmail.com>
2022-03-11 17:02:35 +0300

    Demo version is ready

--- PersonApp/Models/User.swift
+++ PersonApp/Models/User.swift
@@ -11,6 +11,7 @@ struct User {
     let userInfo: Profile
     let aboutUser: AboutUser
     let education: Education
+    let sport: Sport
     
     static func getUserInfo() -> User {
         User(
@@ -18,7 +19,8 @@ struct User {
             password: "qwe",
             userInfo: Profile.getUserInfo(),
             aboutUser: AboutUser.getAboutUserInfo(),
-            education: Education.getEducation()
+            education: Education.getEducation(),
+            sport: Sport.getSport()
         )
     }
 }
@@ -69,14 +71,85 @@ struct Education {
     
     static func getEducation() -> Education {
         Education(
-            university: "Bauman Moscow State Technical University",
+            university: "Bauman Moscow State Technical University 🥵",
             faculties: "Mechanical Engineering Technologies",
-            departments: "Welding and non-destructive technologies",
-            specialty: "Specialist in the field of diagnostics and life extension of welded structures",
+            departments: "Welding and non-destructive technologies 👨‍🏭",
+            specialty: "Specialist in the field of diagnostics and life extension of welded structures ✅",
             yearsOfStudy: 6,
-            topicOfDiploma: "Technology of laser-ultrasonic control of the cooling path of the nozzle for a liquid rocket engine",
+            topicOfDiploma: "Technology of laser-ultrasonic control of the cooling path of the nozzle for a liquid rocket engine 🚀",
             averageMark: 4.2,
             graduationYear: 2021
         )
+    }
+}
+
+struct Sport {
+    let type: [String]
+    let description: [String]
+    
+    static func getSport() -> Sport {
+        Sport(
+            type: [
+                "🥋 Karate 🥋",
+                "🏂 Snowboarding 🏂",
+                "🛹 Skateboarding 🛹",
+                "🎾 Tennis 🎾",
+                "⚽️ Football ⚽️",
+                "🏀 Basketball 🏀",
+                "🏔 Climbing 🏔"
+            ],
+        description: ["""
+            • Experience: 10 years
+            
+            • Black belt - 1 dan.
+            • More than 100 medals (gold/silver/bronze)
+            • Winner and medalist of the World Cup
+            • Winner of Russian Championships
+            • Winner and prize winner in more than 10 countries
+            """,
+            """
+            • Experience: 12 years
+            
+            • Trained more than 10 people from scratch in 3 days
+            • Can freestyle on a flat
+            • Riding in a snow park
+            • Have a cool DC board
+            • I listen to rock while riding
+            """,
+            """
+            • Experience: 6 years
+            
+            • Can freestyle on a flat
+            • Have a cool Russian custom board
+            • I listen to rap while riding
+            """,
+            """
+            • Experience: 2 years
+            
+            • Racket - Yonex
+            • Favorite player - Nick Kiryos
+            • Learned to play in a closed city - cosmodrome (Plesetsk)
+            """,
+            """
+            • Experience: 18 years
+            
+            • My favourite club is Barcelona since 2008
+            • Favourite National Team: Spain since 2008
+            • Best Players: Messi, Fernando Torres and David Villa (no one to choose)
+            • Played since childhood and still do
+            """,
+            """
+            • Experience: 6 years
+            
+            • Played since childhood and still do
+            • Even my parents beat me...
+            """,
+            """
+            • Experience: 2 years
+            
+            • I've been to Cape Bold twice, on Sakhalin Island.
+            • This is just the beginning!
+            """
+        ])
     }
 }
--- PersonApp/ViewControllers/SportViewController.swift
+++ PersonApp/ViewControllers/SportViewController.swift
@@ -9,9 +9,70 @@ import UIKit
 
 class SportViewController: UIViewController {
     
+    @IBOutlet var typeOfSportTextField: UITextField!
+    @IBOutlet var descriptionLabel: UILabel!
+    
     var user: User!
+    
+    private var selectedElement: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        descriptionLabel.text = ""
+        
+        choiceTypeOfSports()
+        createToolbar()
+    }
+    
+    func choiceTypeOfSports() {
+        
+        let elementPicker = UIPickerView()
+        elementPicker.delegate = self
+        
+        typeOfSportTextField.inputView = elementPicker
+    }
+    
+    func createToolbar() {
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        
+        typeOfSportTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension SportViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return user.sport.type.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return user.sport.type[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        selectedElement = user.sport.type[row]
+        typeOfSportTextField.text = selectedElement
+        selectedElement = String(user.sport.description[row])
+        descriptionLabel.text = selectedElement
     }
 }
